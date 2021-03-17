aa3a67fb6f4173ea131434efe2e207ca122d7bbd
David Wengier <git@wengier.com>
2021-03-17 21:46:15 +1100

    Initial commit

---
+++ AssemblyResolver.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ICSharpCode.Decompiler.Metadata;
+
+namespace DecompilationDiffer
+{
+    internal class AssemblyResolver : IAssemblyResolver
+    {
+        private readonly List<PEFile> _peFiles;
+        private readonly List<Stream> _streams;
+
+        public AssemblyResolver(List<(string Name, Stream Stream)> references)
+        {
+            _peFiles = new List<PEFile>();
+            _streams = new List<Stream>();
+            foreach (var reference in references)
+            {
+                _peFiles.Add(new PEFile(reference.Name, reference.Stream));
+                reference.Stream.Seek(0, SeekOrigin.Begin);
+                _streams.Add(reference.Stream);
+            }
+        }
+
+        public PEFile Resolve(IAssemblyReference reference)
+        {
+            return _peFiles.FirstOrDefault(r => r.FullName == reference.FullName);
+        }
+
+        public Task<PEFile> ResolveAsync(IAssemblyReference reference)
+        {
+            return Task.FromResult(_peFiles.FirstOrDefault(r => r.FullName == reference.FullName));
+        }
+
+        public PEFile ResolveModule(PEFile mainModule, string moduleName)
+        {
+            return null;
+        }
+
+        public Task<PEFile> ResolveModuleAsync(PEFile mainModule, string moduleName)
+        {
+            return Task.FromResult<PEFile>(null);
+        }
+
+        internal IEnumerable<Stream> GetAllStreams()
+        {
+            return _streams;
+        }
+    }
+}
---
+++ Runner.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using ICSharpCode.Decompiler;
+using ICSharpCode.Decompiler.Metadata;
+using System.Diagnostics;
+
+#nullable enable
+
+namespace DecompilationDiffer
+{
+    internal class Runner
+    {
+        private DecompilerSettings _decompilerSettings = new DecompilerSettings(ICSharpCode.Decompiler.CSharp.LanguageVersion.CSharp1)
+        {
+            ArrayInitializers = false,
+            AutomaticEvents = false,
+            DecimalConstants = false,
+            FixedBuffers = false,
+            UsingStatement = false,
+            SwitchStatementOnString = false,
+            LockStatement = false,
+            ForStatement = false,
+            ForEachStatement = false,
+            SparseIntegerSwitch = false,
+            DoWhileStatement = false,
+            StringConcat = false,
+            UseRefLocalsForAccurateOrderOfEvaluation = true,
+            InitAccessors = true,
+            FunctionPointers = true,
+            NativeIntegers = true
+        };
+        private static List<MetadataReference>? s_references;
+        private static AssemblyResolver? s_assemblyResolver;
+        private readonly string _baseCode;
+        private readonly string _version1;
+
+        public string ErrorText { get; private set; } = "";
+        public string Version1Output { get; private set; } = "";
+        public string BaseOutput { get; private set; } = "";
+
+        public Runner(string baseCode, string version1)
+        {
+            _baseCode = baseCode;
+            _version1 = version1;
+        }
+
+        internal async Task Run(string baseUri)
+        {
+            try
+            {
+                if (s_references == null)
+                {
+                    s_assemblyResolver = new AssemblyResolver(await GetReferenceStreams(baseUri));
+                    s_references = GetReferences(s_assemblyResolver);
+                }
+
+                this.BaseOutput = "";
+                this.Version1Output = "";
+                this.ErrorText = "";
+
+                if (string.IsNullOrWhiteSpace(_baseCode) || string.IsNullOrWhiteSpace(_version1))
+                {
+                    this.ErrorText = "Need more input!";
+                    return;
+                }
+
+                this.BaseOutput = CompileAndDecompile(_baseCode, out string errors);
+                if (errors is not null)
+                {
+                    this.ErrorText = errors;
+                    return;
+                }
+                this.Version1Output = CompileAndDecompile(_version1, out errors);
+                if (errors is not null)
+                {
+                    this.ErrorText = errors;
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                this.ErrorText = "Error doing something: " + ex.ToString();
+            }
+        }
+
+        private string? CompileAndDecompile(string code, out string? errors)
+        {
+            SyntaxTree? codeTree = CSharpSyntaxTree.ParseText(code, new CSharpParseOptions(kind: SourceCodeKind.Regular), "Program.cs");
+            var codeCompilation = CSharpCompilation.Create("Program", new SyntaxTree[] { codeTree }, s_references, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            errors = GetErrors("Error(s) compiling program:", codeCompilation.GetDiagnostics());
+            if (errors != null)
+            {
+                return null;
+            }
+
+            var assemblyStream = GetAssemblyStream(codeCompilation, "Code", out var rawErrors);
+            if (rawErrors is { Length: > 0 } || assemblyStream == null)
+            {
+                errors = "Error getting assembly stream: " + rawErrors;
+                return null;
+            }
+            using var peFile = new PEFile("", assemblyStream);
+            var decompiler = new ICSharpCode.Decompiler.CSharp.CSharpDecompiler(peFile, s_assemblyResolver, _decompilerSettings);
+            return decompiler.DecompileWholeModuleAsString();
+        }
+
+        private Stream? GetAssemblyStream(Compilation generatorCompilation, string name, out string? errors)
+        {
+            try
+            {
+                var generatorStream = new MemoryStream();
+                Microsoft.CodeAnalysis.Emit.EmitResult? result = generatorCompilation.Emit(generatorStream);
+                if (!result.Success)
+                {
+                    errors = GetErrors($"Error emitting {name}:", result.Diagnostics, false);
+                    return null;
+                }
+                generatorStream.Seek(0, SeekOrigin.Begin);
+                errors = null;
+                return generatorStream;
+            }
+            catch (Exception ex)
+            {
+                errors = ex.ToString();
+                return null;
+            }
+        }
+
+        private static string? GetErrors(string header, IEnumerable<Diagnostic> diagnostics, bool errorsOnly = true)
+        {
+            IEnumerable<Diagnostic>? errors = diagnostics.Where(d => !errorsOnly || d.Severity == DiagnosticSeverity.Error);
+
+            if (!errors.Any())
+            {
+                return null;
+            }
+
+            return header + Environment.NewLine + Environment.NewLine + string.Join(Environment.NewLine, errors);
+        }
+
+        private static List<MetadataReference> GetReferences(AssemblyResolver assemblyResolver)
+        {
+            var references = new List<MetadataReference>();
+
+            foreach (Stream stream in assemblyResolver.GetAllStreams())
+            {
+                stream.Seek(0, SeekOrigin.Begin);
+                references.Add(MetadataReference.CreateFromStream(stream));
+            }
+
+            return references;
+        }
+
+        private static async Task<List<(string, Stream)>> GetReferenceStreams(string baseUri)
+        {
+            Assembly[]? refs = AppDomain.CurrentDomain.GetAssemblies();
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(baseUri)
+            };
+
+            var references = new List<(string, Stream)>();
+
+            foreach (Assembly? reference in refs.Where(x => !x.IsDynamic && !string.IsNullOrWhiteSpace(x.Location)))
+            {
+                Stream? stream = await client.GetStreamAsync($"_framework/_bin/{reference.Location}");
+                references.Add((reference.FullName, stream));
+
+            }
+
+            return references;
+        }
+    }
+}
