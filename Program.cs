0dcfee769c35041cce158ca7506f8593de814a54
David Wengier <git@wengier.com>
2021-03-17 23:08:13 +1100

    Add some defaults

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace DecompilationDiffer
 {
@@ -12,8 +9,6 @@ namespace DecompilationDiffer
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
-            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
