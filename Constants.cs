66ea119422c414f16b883fb0ddd1dc709b2f5db3
David Wengier <git@wengier.com>
2024-07-26 10:00:23 +1000

    More modern defaults

---
+++ Constants.cs
@@ -0,0 +1,49 @@
+﻿namespace DecompilationDiffer;
+
+public static class Constants
+{
+    public const string InitialCode = """
+        using System.Collections.Generic;
+        using System.Linq;
+
+        class C
+        {
+            private IEnumerable<int> _data = M(new [] {1, 2, 3});
+
+            static IEnumerable<int> M(IEnumerable<int> input)
+            {
+                return input.Select(x => x);
+            }
+        }
+        """;
+
+    public const string Version1Code = """
+        using System.Collections.Generic;
+        using System.Linq;
+
+        class C
+        {
+            private IEnumerable<int> _data = M([1, 2, 3]);
+
+            static IEnumerable<int> M(IEnumerable<int> input)
+            {
+                return input.Select(x => x);
+            }
+        }
+        """;
+
+    public const string Version2Code = """
+        using System.Collections.Generic;
+        using System.Linq;
+
+        class C
+        {
+            private IEnumerable<int> _data = M(1, 2, 3);
+
+            static IEnumerable<int> M(params IEnumerable<int> input)
+            {
+                return input.Select(x => x);
+            }
+        }
+        """;
+}
